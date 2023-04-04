6e67a7dfd107d28780c41b7366410006e2f7280c
Mijael Yen Quispe <mijael.yq2002@gmail.com>
2023-04-03 22:13:23 -0500

    inicio proyecto

---
+++ vue-project/src/components/HeaderProject.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class ="container">
+      <div class="my-class">My component {{name}}</div>
+        <div>
+            <div class="my-class"></div>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['name'],
+    name: "header"
+}
+</script>
+
+<style scoped>
+.my-class{
+    background-color: red;
+    margin-top: 10px
+}
+
+.container{
+    display: flex;
+}
+</style>
