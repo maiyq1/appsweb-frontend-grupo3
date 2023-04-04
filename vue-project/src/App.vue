74c8608fbd8ba557ce964b55546d60a783ad17a8
Mijael Yen Quispe <mijael.yq2002@gmail.com>
2023-04-03 22:31:34 -0500

    navbar

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,21 +2,22 @@
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import HeaderProject from "./components/HeaderProject.vue";
+import navbar from "./components/navbar.vue";
+import Navbar from "./components/navbar.vue";
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
+  </header> -->
+  <header>
+      <navbar/>
   </header>
-
   <main>
-    <TheWelcome />
-      <HeaderProject name ="Componente 1"/>
-      <HeaderProject name ="Componente 2"/>
   </main>
 </template>
 
---
+++ vue-project/src/components/navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+      <li><a href="#Menu">Menu</a></li>
+      <li><a href="Title">Library UPC</a></li>
+      <li><a href="MyProfile">My Profile</a></li>
+  </ul>
+</template>
+
+<script>
+export default {
+    props: ['section'],
+    name: "navbar"
+}
+</script>
+
+<style scoped>
+ul li a{
+    border: 1px solid deepskyblue;
+}
+ul{
+    display: flex;
+    list-style-type: none;
+    border: 1px solid red;
+    justify-content: space-between;
+}
+</style>
