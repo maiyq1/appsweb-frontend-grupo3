54edf5427df33e0b1cfb156dcd0c691798a0ff90
U201910146 <u201910146@upc.edu.pe>
2023-04-03 22:43:03 -0500

    Added Footer to page

---
+++ vue-project/src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>
+    <p>{{ mail }}</p>
+    <p>{{ telefono }}</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+footer {
+  background-color: white;
+  display: inline;
+  justify-content: space-between;
+  align-items: center;
+}
+
+</style>
