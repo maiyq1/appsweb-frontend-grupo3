336ef421786de21f353a828baea24b182c805416
CDenisse97 <u201513556@upc.edu.pe>
2023-04-07 03:48:54 -0500

    Update Categories
    Hacer el componente dinámico.

--- vue-project/src/components/Categories.vue
+++ vue-project/src/components/Categories.vue
@@ -3,8 +3,9 @@
         <div class="category-grid">
             <div class="category">
                 <h3>Tutorial 1</h3>
-                <img  style="width: 70%" src="https://www.shutterstock.com/image-photo/webinar-online-education-on-internet-260nw-1931033117.jpg">
-                <p>Description of category 1</p>
+                <img style="width: 70%" src="https://www.shutterstock.com/image-photo/webinar-online-education-on-internet-260nw-1931033117.jpg">
+                <p>{{ description }}</p>
+                <button @click="handleClick">Click me</button>
             </div>
         </div>
     </div>
@@ -13,8 +14,62 @@
 <script>
 export default {
     name: "categories",
+    data() {
+        return {
+            description: "Description of category 1"
+        };
+    },
+    mounted() {
+        console.log("Component mounted");
+    },
+    updated() {
+        console.log("Component updated");
+    },
+    beforeUnmount() {
+        console.log("Component about to be unmounted");
+    },
+    unmounted() {
+        console.log("Component unmounted");
+    },
+    methods: {
+        handleClick() {
+            this.description = "New description!";
+        }
+    }
 };
 </script>
+
+<style scoped>
+.sidebar {
+}
+
+.category-grid {
+    display: grid;
+}
+
+.category {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #f2f2f2;
+    padding: 20px;
+    text-align: center;
+}
+
+.category h3 {
+    font-size: 18px;
+    margin-bottom: 10px;
+    color: black;
+}
+
+.category p {
+    font-size: 14px;
+    line-height: 1.5;
+    color: #2c3e50;
+}
+</style>
+
 
 <style scoped>
 .sidebar {
