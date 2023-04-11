c5b166ba695cd98eb63bb8fc8cf77d9353e1f2cc
JohnPaulM@m@ni <66883063+johnpaulmamaniquispe@users.noreply.github.com>
2023-04-11 10:15:04 -0500

    implementacion con primevue
    implementacion con primevue

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,6 +1,43 @@
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+//PrimeVue
+import PrimeVue from 'primevue/config';
+
+import "primevue/resources/themes/lara-light-indigo/theme.css";//theme
+//import "primevue/resources/themes/arya-orange/theme.css"
+//import "primevue/resources/themes/vela-green/theme.css"
+import "primevue/resources/primevue.min.css"; //core
+import "primeicons/primeicons.css"; //icons
+import Button from "primevue/button"
+
+import InputMask from "primevue/inputmask"
+import Panel from 'primevue/panel';
+import AutoComplete from 'primevue/autocomplete';
+import Dropdown from 'primevue/dropdown';
+import InputNumber from 'primevue/inputnumber';
+import Textarea from 'primevue/textarea';
+import TriStateCheckbox from 'primevue/tristatecheckbox';
+import ConfirmPopup from 'primevue/confirmpopup';
+import RadioButton from 'primevue/radiobutton';
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+
+app.component('InputMask', InputMask);
+app.component('Panel', Panel);
+app.component('AutoComplete', AutoComplete);
+app.component('Dropdown', Dropdown);
+app.component('InputNumber', InputNumber);
+app.component('Textarea', Textarea);
+app.component('TrisStateCheckbox', TriStateCheckbox);
+app.component('ConfirmPopup', ConfirmPopup);
+app.component('Button', Button);
+app.component('RadioButton', RadioButton);
+
+
+app.mount('#app')
