3e8e1e08dfe275672a7232f03ee1c167d3a862ff
JohnPaulM@m@ni <66883063+JohnPaulMamaniQuispe@users.noreply.github.com>
2023-04-11 11:44:25 -0500

    implementacion con primevue
    implementacion con primevue(laboratory 4)

--- vue-project/src/components/listComponent.vue
+++ vue-project/src/components/listComponent.vue
@@ -1,7 +1,7 @@
 <template>
 
-  <div v-for="category in categories ">
-    <Sidebar :name=category.name />
+  <div v-for="category in categories">
+    <Sidebar :name=category.name :description=category.description   />
   </div>
 
 </template>
@@ -15,13 +15,15 @@ export default {
     return{
       categories:[]
     }},
-  BeforeMount(){
+  beforeMount(){
     //invoco API
-    this.categories =[{ name:'Category 1',description:'This section has description of'},{ name:'Category 2',description:'This section has description of'},{ name:'Category 3',description:'This section has description of'}]
+    this.categories =[{ name:'Category 1',description:'This section has description of'},
+      { name:'Category 2',description:'This section has description of'},
+      { name:'Category 3',description:'This section has description of'},
+      { name:'Category 4',description:'This section has description of'}]
   }
 }
 </script>
-
 <style scoped>
 
 </style>
